b089dbc0e778f34afecbd629326bddcfe28d813e
linjiangxiong <linjx2032@gmail.com>
2022-03-10 11:34:44 +0800

    First Commit

---
+++ foreachargs.scala
@@ -0,0 +1,9 @@
+println("args.foreach(arg => println(arg))")
+args.foreach(arg => println(arg))
+
+println("args.foreach(println)")
+args.foreach(println)
+
+println("args.foreach((arg: String) => println(arg))")
+args.foreach((arg: String) => println(arg))
+
---
+++ hello.scala
@@ -0,0 +1,1 @@
+println("from a script.")
